2a69d86ab5e68eed20db1c8bb508e8dd80b67752
6704062617041 <s6704062617041@email.kmutnb.ac.th>
2024-11-28 17:53:26 +0700

    else

--- lab1minx.c
+++ lab1minx.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 int main(){
-	int x = 20;
-	int y = 10;
+	int x = 15;
+	int y = 20;
 	if (x>y){
 		printf("x is greater than y");
-	}
-	if (y>x){
+	} else{
 		printf("y is greater than x");
 	}
 }
