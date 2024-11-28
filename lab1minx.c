0ed07ee7b77b7feea338a804cb125c1ef165b715
6704062617041 <s6704062617041@email.kmutnb.ac.th>
2024-11-28 18:07:46 +0700

    ข้อ 2

--- lab1minx.c
+++ lab1minx.c
@@ -5,7 +5,11 @@ int main(){
 	int y = 20;
 	if (x>y){
 		printf("x is greater than y");
+		//diff of x and y
+		printf("\n%d",(x-y));
 	} else{
 		printf("y is greater than x");
+		//diff of x and y
+		printf("\n%d",(y-x));
 	}
 }
